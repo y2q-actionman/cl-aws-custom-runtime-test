509f6f0fab8fb1fd9d4e5d71a474846e4c55368e
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-02-14 00:34:35 +0900

    supported invocation/init error API

--- bootstrap/aws-bootstrap.asd
+++ bootstrap/aws-bootstrap.asd
@@ -6,11 +6,9 @@
   :author "YOKOTA Yuki <y2q.actionman@gmail.com>"
   :depends-on (:drakma :usocket)
   :components
-  ((:module "src"
-    :components
-    ((:file "package")
-     (:file "usocket-patch-drop-ipv6" :depends-on ("package"))
-     (:file "lambda-env-vars" :depends-on ("package"))
-     (:file "find-handler" :depends-on ("package"))
-     (:file "bootstrap"
-	    :depends-on ("usocket-patch-drop-ipv6" "lambda-env-vars" "find-handler"))))))
+  ((:file "package")
+   (:file "usocket-patch-drop-ipv6" :depends-on ("package"))
+   (:file "lambda-env-vars" :depends-on ("package"))
+   (:file "find-handler" :depends-on ("package"))
+   (:file "bootstrap"
+	  :depends-on ("usocket-patch-drop-ipv6" "lambda-env-vars" "find-handler"))))
--- bootstrap/bootstrap.lisp
+++ bootstrap/bootstrap.lisp
@@ -1,29 +1,82 @@
+;;; This file implements AWS Lambda Runtime Interface.
+;;; See: https://docs.aws.amazon.com/lambda/latest/dg/runtimes-api.html
+
 (in-package :aws-bootstrap)
 
+(defvar *next-invocation-path*
+  (load-time-value
+   (format nil "http://~A/2018-06-01/runtime/invocation/next"
+	   *AWS-LAMBDA-RUNTIME-API*))
+  "An URI for getting a next event.")
+
+(defun make-invocation-response-path (request-id)
+  "Makes an URI for for reporting invocation response"
+  (format nil "http://~A/2018-06-01/runtime/invocation/~A/response"
+	  *AWS-LAMBDA-RUNTIME-API* request-id))
+
+(defun make-invocation-error-path (request-id)
+  "Makes an URI for for reporting invocation error"
+  (format nil "http://~A/2018-06-01/runtime/invocation/~A/error"
+	  *AWS-LAMBDA-RUNTIME-API* request-id))
+
+(defvar *initialization-error-path*
+  (load-time-value
+   (format nil "http://~A/2018-06-01/runtime/init/error" *AWS-LAMBDA-RUNTIME-API*))
+  "An URI for reporing initialization error.")
+
+(defun make-error-response-contents (condition)
+  "Makes JSON string used for reporting invocation or initialization errors"
+  (let ((error-message (princ-to-string condition))
+	(error-type (type-of condition)))
+    (format nil "{\"errorMessage\" : \"~A\", \"errorType\" : \"~A\"}"
+	    error-message error-type)))
+
+
 (defun main-loop (handler)
-  (let ((next-endpoint
-	 (format nil "http://~A/2018-06-01/runtime/invocation/next" *AWS-LAMBDA-RUNTIME-API*)))
-    (loop
-       for (body status headers . nil)
-	 = (multiple-value-list (drakma:http-request next-endpoint))
-       as request-id = (cdr (assoc :Lambda-Runtime-Aws-Request-Id headers))
-       as response = (funcall handler body headers)
-       as response-endpoint
-	 = (format nil "http://~A/2018-06-01/runtime/invocation/~A/response"
-		   *AWS-LAMBDA-RUNTIME-API* request-id)
-       do (drakma:http-request response-endpoint
-			       :method :POST
-			       :content response))))
+  "This custom runtime's main loop.
+This function contiues to retrieve an event, funcall `handler' with ,
+and send its result back."
+  (loop for (body status headers . nil)
+       = (multiple-value-list (drakma:http-request *next-invocation-path*))
+     as request-id = (cdr (assoc :Lambda-Runtime-Aws-Request-Id headers))
+     as response-path = (make-invocation-response-path request-id) ; TODO: buffering
+     as error-path = (make-invocation-error-path request-id) ; TODO: buffering
+     do (handler-case
+	    (let ((response (funcall handler body headers)))
+	      (drakma:http-request response-path
+				   :method :POST
+				   :content response))
+	  (error (condition)
+	    (drakma:http-request *initialization-error-path*
+				 :method :POST
+				 :content (make-error-response-contents condition))))))
 
 (defun bootstrap ()
-  (patch-get-hosts-by-name)
-  
-  (setf drakma:*drakma-default-external-format* :utf-8)
-  ;; For seeing JSON as text. (https://blog.kyanny.me/entry/2017/08/18/031415)
-  (push '("application" . "json") drakma:*text-content-types*)
+  "Initialize this runtime, find a handler function, and go to `main-loop'."
+  (let (handler)
+    (handler-case
+	(progn
+	  (setf drakma:*drakma-default-external-format* :utf-8)
+	  
+	  ;; For seeing JSON as text. (https://blog.kyanny.me/entry/2017/08/18/031415)
+	  (push '("application" . "json") drakma:*text-content-types*)
+	  
+	  ;; To drop IPv6 support, I patch `usocket:get-hosts-by-name'.
+	  (patch-get-hosts-by-name)
 
-  (load-aws-lambda-environmental-variables)
-  (sb-posix:chdir *LAMBDA-TASK-ROOT*)	; TODO: use this in `find-handler'
+	  ;; Move cwd to the AWS lambda function code.
+	  ;; I think this is convetnient to `load' them.
+	  (sb-posix:chdir *LAMBDA-TASK-ROOT*) 
 
-  ;; goto main loop
-  (main-loop (find-handler *_HANDLER*)))
+	  ;; find this function's handler
+	  (setf handler (or (find-handler *_HANDLER*)
+			    (error "No handler found. (specified by ~A)" *_HANDLER*))))
+      (error (condition)
+	;; Calls AWS Lambda's initialization error API.
+	(drakma:http-request *initialization-error-path*
+			     :method :POST
+			     :content (make-error-response-contents condition))
+	;; and exits immediately.
+	(return-from bootstrap nil)))
+    ;; goto main loop
+    (main-loop handler)))
