da8f2af3c8dd6fe75925d3bc585291cf11b54aba
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-02-14 07:12:46 +0900

    added debug comment to ipv6 code

--- aws-lambda-runtime/usocket-patch-drop-ipv6.lisp
+++ aws-lambda-runtime/usocket-patch-drop-ipv6.lisp
@@ -4,18 +4,30 @@
 
 (in-package :aws-lambda-runtime)
 
+(defvar *original-get-host-by-name* nil)
+
 (defun get-hosts-by-name-and-remove-ipv6 (name)
-  "Calls the original `usocket:get-hosts-by-name' (via `original-get-host-by-name')
+  "Calls the original `usocket:get-hosts-by-name' (via `*original-get-host-by-name*')
 and removes IPv6 addresses, for supress making IPv6 sockets."
-  (loop with all-addrs = (funcall 'original-get-host-by-name name)
+  (loop with all-addrs = (funcall *original-get-host-by-name* name)
      for addr in all-addrs
      unless (= 16 (length addr))	; see usocket's sbcl code.
-     collect addr))
+     collect addr into ret
+     finally
+       (let ((*print-pretty* nil))
+	 (format *debug-io* "~&addrs ~A -> ~A~%" all-addrs ret))
+       (return ret)))
 
 (defun patch-get-hosts-by-name ()
   "Replace `usocket:get-hosts-by-name' with our `get-hosts-by-name-and-remove-ipv6'"
-  (unless (fboundp 'original-get-host-by-name)
-    (setf (fdefinition 'original-get-host-by-name) ; saves the original one.
-	  (fdefinition 'usocket:get-hosts-by-name))
-    (setf (fdefinition 'usocket:get-hosts-by-name) ; overwrite with mine.
-	  (fdefinition 'get-hosts-by-name-and-remove-ipv6))))
+  (unless *original-get-host-by-name*
+    (setf *original-get-host-by-name*
+	  (fdefinition 'usocket:get-hosts-by-name)))
+  (shiftf (fdefinition 'usocket:get-hosts-by-name)
+	  (fdefinition 'get-hosts-by-name-and-remove-ipv6)))
+
+
+(eval-when (:load-toplevel :execute)
+  (trace get-hosts-by-name-and-remove-ipv6)
+  (trace usocket::host-to-hostname)
+  (trace usocket:get-hosts-by-name))
