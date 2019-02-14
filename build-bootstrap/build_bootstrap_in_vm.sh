f8f4620743178d097e4380fefaa8b393804c4534
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-02-15 00:22:36 +0900

    added roswell script into bootstrap

--- build-bootstrap/build_bootstrap_in_vm.sh
+++ build-bootstrap/build_bootstrap_in_vm.sh
@@ -5,6 +5,8 @@ BIN_NAME="bootstrap"
 /usr/local/bin/sbcl \
     --non-interactive \
     --load "/work/ql_libs_at_docker_build.lisp" \
+    --eval "(asdf:load-asd \"/work/roswell-19.1.10.96/roswell.asd\")" \
+    --eval "(asdf:load-system \"roswell\")" \
     --load "/aws-lambda-runtime/aws-lambda-runtime.asd" \
     --eval "(asdf:load-system :aws-lambda-runtime)" \
     --eval "(sb-ext:save-lisp-and-die \"$BIN_NAME\" :executable t :toplevel 'aws-lambda-runtime::bootstrap)"
--- build-bootstrap/ql_libs_at_docker_build.lisp
+++ build-bootstrap/ql_libs_at_docker_build.lisp
@@ -1,3 +1,10 @@
+;;; for bootstrap ifself.
 (ql:quickload "alexandria")
 (ql:quickload "drakma")
 (ql:quickload "uiop")
+
+;;; for loading Roswell
+(ql:quickload "simple-date-time")
+(ql:quickload "plump")
+(ql:quickload "split-sequence")
+(ql:quickload "zip")
