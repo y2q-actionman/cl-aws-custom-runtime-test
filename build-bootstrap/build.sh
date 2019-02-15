7fee6d0fc7b9e52772fbfa88d8c7a29a4facd1a3
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-02-15 14:08:28 +0900

    merged runtime build script

--- build-bootstrap/build.sh
+++ build-bootstrap/build.sh
@@ -2,6 +2,8 @@
 
 cd `dirname $0`
 
+LAYER_NAME="lisp-layer"
+
 # build VM.
 docker build -t test .
 
@@ -10,3 +12,8 @@ docker run --rm \
        -v `pwd`:/out \
        -v `pwd`/../aws-lambda-runtime:/aws-lambda-runtime \
        test /out/build_bootstrap_in_vm.sh
+
+# publish as a AWS custom function layer
+aws lambda publish-layer-version \
+    --zip-file fileb://aws_lambda_bootstrap.zip \
+    --layer-name $LAYER_NAME
--- build-bootstrap/publish_aws_lambda_layer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-LAYER_NAME="lisp-layer"
-
-aws lambda publish-layer-version \
-    --zip-file fileb://aws_lambda_bootstrap.zip \
-    --layer-name $LAYER_NAME
