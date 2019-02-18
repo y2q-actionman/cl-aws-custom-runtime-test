9b4178bf2fe06f52695b80512abd17ca0fd5b682
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-02-18 13:46:10 +0900

    added json libs at docket build

--- build-bootstrap/ql_libs_at_docker_build.lisp
+++ build-bootstrap/ql_libs_at_docker_build.lisp
@@ -1,10 +1,21 @@
 ;;; for bootstrap ifself.
-(ql:quickload "alexandria")
-(ql:quickload "drakma")
-(ql:quickload "uiop")
+(ql:quickload :alexandria)
+(ql:quickload :drakma)
+(ql:quickload :uiop)
+(ql:quickload :cl-fad)
 
 ;;; for loading Roswell
-(ql:quickload "simple-date-time")
-(ql:quickload "plump")
-(ql:quickload "split-sequence")
-(ql:quickload "zip")
+(ql:quickload :simple-date-time)
+(ql:quickload :plump)
+(ql:quickload :split-sequence)
+(ql:quickload :zip)
+
+;;; JSON Libs
+;;; See: https://sites.google.com/site/sabraonthehill/home/json-libraries
+(ql:quickload :cl-json)
+(ql:quickload :st-json)
+(ql:quickload :yason)
+(ql:quickload :jsown)
+(ql:quickload :jonathan)   ; I surprised this lib has 8 dependencies.
+(ql:quickload :json-streams)
+(ql:quickload :com.gigamonkeys.json)
