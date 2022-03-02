46859b309b3ac2dcc852032ccf1a7c28f98c6ec0
Simon Janes <spjanes@protonmail.com>
2022-03-02 08:45:14 -0600

    Optional layer name

--- build_and_publish_custom_runtime.sh
+++ build_and_publish_custom_runtime.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+    _layer_name="lisp-layer"
+else
+    _layer_name=$1
+fi
+
+if [ -z "$2" ]; then
+    _sbcl_version="2.2.2"
+else
+    _sbcl_version=$1
+fi
+
+
 cd `dirname $0`
 
 # build a VM.
@@ -13,7 +26,7 @@ docker run --rm \
        $VM_NAME /out/build_bootstrap_in_vm.sh $ZIP_NAME
 
 # publish as a AWS custom function layer
-LAYER_NAME="lisp-layer"
+LAYER_NAME="$_layer_name"
 aws lambda publish-layer-version \
     --zip-file fileb://build-bootstrap-out/$ZIP_NAME \
     --layer-name $LAYER_NAME
